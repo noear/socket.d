499060228d39b4c07ccfab8e3050b5c0ea2a1643
noear <noear@live.cn>
2023-11-01 00:33:04 +0800

    添加 ListenerPipeline 接口

---
+++ socketd/src/main/java/org/noear/socketd/protocol/ListenerPipeline.java
@@ -0,0 +1,53 @@
+package org.noear.socketd.protocol;
+
+import java.io.IOException;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 监听管道
+ *
+ * @author noear
+ * @since 2.0
+ */
+public class ListenerPipeline implements Listener {
+    Deque<Listener> deque = new LinkedList<>();
+
+    public ListenerPipeline prev(Listener listener) {
+        deque.addFirst(listener);
+        return this;
+    }
+
+    public ListenerPipeline next(Listener listener) {
+        deque.addLast(listener);
+        return this;
+    }
+
+    @Override
+    public void onOpen(Session session) {
+        for (Listener listener : deque) {
+            listener.onOpen(session);
+        }
+    }
+
+    @Override
+    public void onMessage(Session session, Message message) throws IOException {
+        for (Listener listener : deque) {
+            listener.onMessage(session, message);
+        }
+    }
+
+    @Override
+    public void onClose(Session session) {
+        for (Listener listener : deque) {
+            listener.onClose(session);
+        }
+    }
+
+    @Override
+    public void onError(Session session, Throwable error) {
+        for (Listener listener : deque) {
+            listener.onError(session, error);
+        }
+    }
+}

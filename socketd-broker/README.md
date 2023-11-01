fb6625d79bd3f2ceeb1b9c178309202c4e105a47
noear <noear@live.cn>
2023-11-01 16:56:45 +0800

    socketd-broker-java-udp 适配完成

--- socketd-broker/README.md
+++ socketd-broker/README.md
@@ -3,12 +3,8 @@
 
 | broker                        | schema  | 支持端 | 备注 |
 |-------------------------------|---------|-----|--|
-| socketd-broker-java-socket    | tcp-bio | c,s |  |
+| socketd-broker-java-tcp       | tcp-bio | c,s |  |
+| socketd-broker-java-udp       | udp     | c,s |  |
 | socketd-broker-java-websocket | ws-bio  | c,s |  |
 | socketd-broker-netty          | tcp-nio | c,s |  |
 | socketd-broker-smartsocket    | tcp-aio | c,s |  |
-|                               |         |     |  |
-|                               |         |     |  |
-|                               |         |     |  |
-|                               |         |     |  |
-|                               |         |     |  |
--- socketd-broker/socketd-broker-java-udp/src/main/java/org/noear/socketd/broker/java_udp/UdpBioChannelAssistant.java
+++ socketd-broker/socketd-broker-java-udp/src/main/java/org/noear/socketd/broker/java_udp/UdpBioChannelAssistant.java
@@ -4,7 +4,6 @@ import org.noear.socketd.broker.java_udp.impl.DatagramFrame;
 import org.noear.socketd.broker.java_udp.impl.DatagramTagert;
 import org.noear.socketd.protocol.ChannelAssistant;
 import org.noear.socketd.protocol.CodecByteBuffer;
-import org.noear.socketd.protocol.Constants;
 import org.noear.socketd.protocol.Frame;
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -19,23 +18,15 @@ import java.nio.ByteBuffer;
  * @since 2.0
  */
 public class UdpBioChannelAssistant implements ChannelAssistant<DatagramTagert> {
+    private static final int LEN_INT = 4;
     private CodecByteBuffer codec = new CodecByteBuffer();
-
-    public int readInt32(byte[] bytes) {
-        int value = 0;
-        for (int i = 0; i < 4; i++) {
-            int shift = (3 - i) * 8;
-            value += (bytes[i] & 0xFF) << shift;
-        }
-        return value;
-    }
 
     /**
      * 读取
      */
     public DatagramFrame read(DatagramSocket source) throws IOException {
         //获取长度
-        DatagramPacket datagramPacket = new DatagramPacket(new byte[Constants.LEN_INT], Constants.LEN_INT);
+        DatagramPacket datagramPacket = new DatagramPacket(new byte[LEN_INT], LEN_INT);
         source.receive(datagramPacket);
         byte[] sizeBytes = datagramPacket.getData();
         if (sizeBytes == null || sizeBytes.length == 0) {
@@ -43,7 +34,7 @@ public class UdpBioChannelAssistant implements ChannelAssistant<DatagramTagert> 
         }
 
         //获取数据（接着在原地址上拿）
-        int size = readInt32(sizeBytes) - Constants.LEN_INT;
+        int size = ByteBuffer.wrap(sizeBytes).getInt();
         datagramPacket = new DatagramPacket(new byte[size], size, datagramPacket.getSocketAddress());
         source.receive(datagramPacket);
         byte[] dataBytes = datagramPacket.getData();
@@ -51,10 +42,7 @@ public class UdpBioChannelAssistant implements ChannelAssistant<DatagramTagert> 
             return null;
         }
 
-        ByteBuffer byteBuffer = ByteBuffer.allocate(size + Constants.LEN_INT);
-        byteBuffer.put(sizeBytes);
-        byteBuffer.put(dataBytes);
-
+        ByteBuffer byteBuffer = ByteBuffer.wrap(dataBytes);
         Frame frame = codec.decode(byteBuffer);
 
         return new DatagramFrame(datagramPacket, frame);
@@ -65,8 +53,14 @@ public class UdpBioChannelAssistant implements ChannelAssistant<DatagramTagert> 
      */
     @Override
     public void write(DatagramTagert target, Frame frame) throws IOException {
-        byte[] bytes = codec.encode(frame).array();
-        target.send(bytes);
+        byte[] dataBytes = codec.encode(frame).array();
+
+        byte[] sizeBytes = ByteBuffer.allocate(LEN_INT).putInt(dataBytes.length).array();
+
+        //先发个长度包
+        target.send(sizeBytes);
+        //再发数据包
+        target.send(dataBytes);
     }
 
     @Override

9226d07accfdba0f296dfbfb07690eb3a8d6715b
noear <noear@live.cn>
2023-11-03 22:32:36 +0800

    完成 sendAndRequest() 超时测试

--- java/socketd-transport-test/src/test/java/features/CaseTest.java
+++ java/socketd-transport-test/src/test/java/features/CaseTest.java
@@ -3,6 +3,7 @@ package features;
 import features.cases.TestCase01;
 import features.cases.TestCase11;
 import features.cases.TestCase12;
+import features.cases.TestCase13_sendAndRequest_timeout;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -18,17 +19,17 @@ public class CaseTest {
     /**
      * 用于调试
      * */
-    public static void main(String[] args) throws Exception {
-        String s1 = schemas[4];
-        TestCase11 testCase01 = new TestCase11(s1, 2000);
-        try {
-            testCase01.start();
-            testCase01.stop();
-        } catch (Exception e) {
-            testCase01.onError();
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) throws Exception {
+//        String s1 = schemas[4];
+//        TestCase11 testCase01 = new TestCase11(s1, 2000);
+//        try {
+//            testCase01.start();
+//            testCase01.stop();
+//        } catch (Exception e) {
+//            testCase01.onError();
+//            e.printStackTrace();
+//        }
+//    }
 
     @Test
     public void testCase01() throws Exception {
@@ -65,6 +66,21 @@ public class CaseTest {
         for (int i = 0; i < schemas.length; i++) {
             String s1 = schemas[i];
             TestCase12 testCase01 = new TestCase12(s1, 3000 + i);
+            try {
+                testCase01.start();
+                testCase01.stop();
+            } catch (Exception e) {
+                testCase01.onError();
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void testCase13() throws Exception {
+        for (int i = 0; i < schemas.length; i++) {
+            String s1 = schemas[i];
+            TestCase13_sendAndRequest_timeout testCase01 = new TestCase13_sendAndRequest_timeout(s1, 4000 + i);
             try {
                 testCase01.start();
                 testCase01.stop();
---
+++ java/socketd-transport-test/src/test/java/features/cases/TestCase13_sendAndRequest_timeout.java
@@ -0,0 +1,86 @@
+package features.cases;
+
+import org.junit.jupiter.api.Assertions;
+import org.noear.socketd.SocketD;
+import org.noear.socketd.exception.SocketdTimeoutException;
+import org.noear.socketd.transport.core.Message;
+import org.noear.socketd.transport.core.Session;
+import org.noear.socketd.transport.core.SimpleListener;
+import org.noear.socketd.transport.core.entity.StringEntity;
+import org.noear.socketd.transport.server.Server;
+import org.noear.socketd.transport.server.ServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * sendAndRequest() 超时
+ *
+ * @author noear
+ * @since 2.0
+ */
+public class TestCase13_sendAndRequest_timeout extends BaseTestCase {
+    private static Logger log = LoggerFactory.getLogger(TestCase13_sendAndRequest_timeout.class);
+    public TestCase13_sendAndRequest_timeout(String schema, int port) {
+        super(schema, port);
+    }
+
+    private Server server;
+    private Session clientSession;
+
+    private AtomicInteger messageCounter = new AtomicInteger();
+
+    @Override
+    public void start() throws Exception {
+        log.trace("...");
+
+        super.start();
+        //server
+        server = SocketD.createServer(new ServerConfig(getSchema()).port(getPort()))
+                .listen(new SimpleListener() {
+                    @Override
+                    public void onMessage(Session session, Message message) throws IOException {
+                        System.out.println("::" + message);
+                        if(message.isRequest()){
+                            messageCounter.incrementAndGet();
+                        }
+                    }
+                })
+                .start();
+
+        //休息下，启动可能要等会儿
+        Thread.sleep(100);
+
+
+        //client
+        String serverUrl = getSchema() + "://127.0.0.1:" + getPort() + "/path?u=a&p=2";
+        clientSession = SocketD.createClient(serverUrl).open();
+
+        clientSession.send("/user/created", new StringEntity("hi"));
+
+        try {
+            clientSession.sendAndRequest("/user/get", new StringEntity("hi"), 100);
+            assert false;
+        }catch (SocketdTimeoutException e){
+            assert true;
+        }
+
+        System.out.println("counter: " + messageCounter.get());
+        Assertions.assertEquals(messageCounter.get(), 1, getSchema() + ":server 收的消息数量对不上");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+
+        if (server != null) {
+            server.stop();
+        }
+
+        if (clientSession != null) {
+            clientSession.close();
+        }
+    }
+}

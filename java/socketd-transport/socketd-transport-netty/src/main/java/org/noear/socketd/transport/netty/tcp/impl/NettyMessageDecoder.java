08627a52295d9b69aa71aa9314971dbe27f251a7
noear <noear@live.cn>
2023-11-05 11:56:11 +0800

    简化 netty 解码器

--- java/socketd-transport/socketd-transport-netty/src/main/java/org/noear/socketd/transport/netty/tcp/impl/NettyMessageDecoder.java
+++ java/socketd-transport/socketd-transport-netty/src/main/java/org/noear/socketd/transport/netty/tcp/impl/NettyMessageDecoder.java
@@ -22,34 +22,22 @@ public class NettyMessageDecoder extends ByteToMessageDecoder {
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf inBuf, List<Object> out) throws Exception {
-        if (inBuf.readableBytes() < Integer.BYTES) {
-            return;
-        }
-
-        inBuf.markReaderIndex();
+        //前面有 LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, -4, 0) 了
         int len = inBuf.readInt();
 
-        if (inBuf.readableBytes() < (len - Integer.BYTES)) {
-            inBuf.resetReaderIndex();
+        byte[] bytes = new byte[len - Integer.BYTES];
+        inBuf.readBytes(bytes);
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(len);
+        byteBuffer.putInt(len);
+        byteBuffer.put(bytes);
+        byteBuffer.flip();
+
+        Frame message = config.getCodec().decode(byteBuffer);
+        if (message == null) {
             return;
         }
 
-        if (len > 0) {
-            byte[] bytes = new byte[len - Integer.BYTES];
-            inBuf.readBytes(bytes);
-
-            ByteBuffer byteBuffer = ByteBuffer.allocate(len);
-            byteBuffer.putInt(len);
-            byteBuffer.put(bytes);
-            byteBuffer.flip();
-
-            Frame message = config.getCodec().decode(byteBuffer);
-            if (message != null) {
-                out.add(message);
-            }
-        }
-
-        //弃用已读的空间，从而复用
-        inBuf.discardReadBytes();
+        out.add(message);
     }
 }

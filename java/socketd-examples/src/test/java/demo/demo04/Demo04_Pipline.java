edb8db30f4495f9a66f5156b5097546215d07e6b
noear <noear@live.cn>
2023-11-07 23:04:56 +0800

    简化 demo 使用

--- java/socketd-examples/src/test/java/demo/demo04/Demo04_Pipline.java
+++ java/socketd-examples/src/test/java/demo/demo04/Demo04_Pipline.java
@@ -3,6 +3,7 @@ package demo.demo04;
 import org.noear.socketd.SocketD;
 import org.noear.socketd.transport.core.*;
 import org.noear.socketd.transport.core.entity.StringEntity;
+import org.noear.socketd.transport.core.listener.BuilderListener;
 import org.noear.socketd.transport.core.listener.PipelineListener;
 import org.noear.socketd.transport.core.listener.SimpleListener;
 import org.noear.socketd.transport.server.ServerConfig;
@@ -13,19 +14,13 @@ public class Demo04_Pipline {
     public static void main(String[] args) throws Throwable {
         //::启动服务端
         SocketD.createServer(new ServerConfig("udp").port(8602))
-                .listen(new PipelineListener().next(new SimpleListener() {
-                    @Override
-                    public void onMessage(Session session, Message message) throws IOException {
-                        //这里可以做拦截
-                        System.out.println("拦截打印::" + message);
-                    }
-                }).next(new SimpleListener() {
-                    @Override
-                    public void onMessage(Session session, Message message) throws IOException {
-                        //这里可以做业务处理
-                        System.out.println(message);
-                    }
-                }))
+                .listen(new PipelineListener().next(new BuilderListener().onMessage((s, m) -> {
+                    //这里可以做拦截
+                    System.out.println("拦截打印::" + m);
+                })).next(new BuilderListener().onMessage((s, m) -> {
+                    //这里可以做业务处理
+                    System.out.println(m);
+                })))
                 .start();
 
         Thread.sleep(1000); //等会儿，确保服务端启动完成

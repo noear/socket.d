2c0ffce87371d6aef263f5087a327512ebf15c25
noear <noear@live.cn>
2023-11-07 23:15:53 +0800

    调整示例

--- java/socketd-examples/src/test/java/demo/demo04/Demo04_Router.java
+++ java/socketd-examples/src/test/java/demo/demo04/Demo04_Router.java
@@ -3,43 +3,38 @@ package demo.demo04;
 import org.noear.socketd.SocketD;
 import org.noear.socketd.transport.core.*;
 import org.noear.socketd.transport.core.entity.StringEntity;
+import org.noear.socketd.transport.core.listener.BuilderListener;
 import org.noear.socketd.transport.core.listener.RouterListener;
 import org.noear.socketd.transport.server.ServerConfig;
 
 public class Demo04_Router {
     public static void main(String[] args) throws Throwable {
         //::启动服务端
-
-        RouterListener router = new RouterListener();
-
-        //用户频道
-        router.of("/").onMessage((s,m)->{
-            System.out.println("user::"+m);
-        });
-
-        //管理员频道
-        router.of("/admin").onOpen(s->{
-            if("admin".equals(s.getHandshake().getParam("u")) == false){
-                s.close();
-            }
-        }).onMessage((s,m)->{
-            System.out.println("admin::"+m);
-        });
-
-
-        SocketD.createServer(new ServerConfig("tcp").port(8602).coreThreads(20))
-                .listen(router)
+        SocketD.createServer(new ServerConfig("tcp").port(8602))
+                .listen(new RouterListener()
+                        .of("/", new BuilderListener().onMessage((s, m) -> {
+                            //用户频道
+                            System.out.println("user::" + m);
+                        }))
+                        .of("/admin", new BuilderListener().onOpen(s -> {
+                            //管理员频道
+                            if ("admin".equals(s.getHandshake().getParam("u")) == false) {
+                                s.close();
+                            }
+                        }).onMessage((s, m) -> {
+                            System.out.println("admin::" + m);
+                        })))
                 .start();
 
         Thread.sleep(1000); //等会儿，确保服务端启动完成
 
         //::打开客户端会话
 
-        //用户频道
+        //用户频道（链接地址的 path ，算为频道）
         Session session1 = SocketD.createClient("tcp://127.0.0.1:8602/?u=a&p=2").open();
         session1.send("/demo", new StringEntity("Hi"));
 
-        //管理员频道
+        //管理员频道（链接地址的 path ，算为频道）
         Session session2 = SocketD.createClient("tcp://127.0.0.1:8602/admin?u=a&p=2").open();
         session2.send("/demo", new StringEntity("Hi"));
     }

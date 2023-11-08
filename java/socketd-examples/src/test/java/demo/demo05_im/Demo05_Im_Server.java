6aab49f6862ce497d2f9a72912d3a3885e92f99a
noear <noear@live.cn>
2023-11-08 23:16:55 +0800

    简化聊天代码

--- java/socketd-examples/src/test/java/demo/demo05_im/Demo05_Im_Server.java
+++ java/socketd-examples/src/test/java/demo/demo05_im/Demo05_Im_Server.java
@@ -1,6 +1,7 @@
 package demo.demo05_im;
 
 import org.noear.socketd.SocketD;
+import org.noear.socketd.transport.core.Entity;
 import org.noear.socketd.transport.core.Session;
 import org.noear.socketd.transport.core.entity.StringEntity;
 import org.noear.socketd.transport.core.listener.BuilderListener;
@@ -15,9 +16,8 @@ import java.util.stream.Collectors;
 
 
 public class Demo05_Im_Server {
+    static Map<String, Session> userList = new HashMap<>();
     public static void main(String[] args) throws Exception {
-        Map<String, Session> userList = new HashMap<>();
-
         SocketD.createServer(new ServerConfig("sd:udp").port(8602))
                 .listen(new RouterListener()
                         //::::::::::用户频道处理
@@ -39,15 +39,7 @@ public class Demo05_Im_Server {
                                     String room = s.getAttr("room");
 
                                     if (Utils.isNotEmpty(room)) {
-                                        for (Session s2 : userList.values().parallelStream()
-                                                .filter(s1 -> room.equals(s.getAttr("room")))
-                                                .collect(Collectors.toList())) {
-
-                                            //给房间的人转发消息
-                                            RunUtils.runAnTry(() -> {
-                                                s2.send("cmd.chat", new StringEntity("新人加入聊天室：" + s.getAttr("user")));
-                                            });
-                                        }
+                                        pushToRoom(room, new StringEntity("有人退出聊天室：" + s.getAttr("user")));
                                     }
                                 }).on("cmd.join", (s, m) -> {
                                     //::加入房间指令
@@ -56,35 +48,17 @@ public class Demo05_Im_Server {
                                     if (Utils.isNotEmpty(room)) {
                                         s.setAttr("room", room);
 
-                                        for (Session s2 : userList.values().parallelStream()
-                                                .filter(s1 -> room.equals(s.getAttr("room")))
-                                                .collect(Collectors.toList())) {
-
-                                            //给房间的人转发消息
-                                            RunUtils.runAnTry(() -> {
-                                                s2.send("cmd.chat", new StringEntity("新人加入聊天室：" + s.getAttr("user")));
-                                            });
-                                        }
+                                        pushToRoom(room, new StringEntity("新人加入聊天室：" + s.getAttr("user")));
                                     }
                                 }).on("cmd.chat", (s, m) -> {
                                     //::聊天指令
                                     String room = m.getMeta("room");
 
                                     if (Utils.isNotEmpty(room)) {
-
                                         StringBuilder buf = new StringBuilder();
-                                        buf.append(m.getMeta("sender")).append(": ")
-                                                .append(m.getDataAsString());
+                                        buf.append(m.getMeta("sender")).append(": ").append(m.getDataAsString());
 
-
-                                        for (Session s2 : userList.values().parallelStream()
-                                                .filter(s1 -> room.equals(s.getAttr("room")))
-                                                .collect(Collectors.toList())) {
-                                            //给房间的人转发消息
-                                            RunUtils.runAnTry(() -> {
-                                                s2.send("cmd.chat", new StringEntity(buf.toString()));
-                                            });
-                                        }
+                                        pushToRoom(room, new StringEntity(buf.toString()));
                                     }
                                 }))
                         //::::::::::管理频道处理
@@ -108,7 +82,17 @@ public class Demo05_Im_Server {
                                         s2.setAttr("room", null);
                                         s2.send("cmd.t", new StringEntity("你被T出聊天室: " + room));
                                     }
-                                })))
-                .start();
+                                })
+                        )
+                ).start();
+    }
+
+    static void pushToRoom(String room, Entity message) {
+        userList.values().parallelStream().filter(s1 -> room.equals(s1.getAttr("room")))
+                .forEach(s1 -> {
+                    RunUtils.runAnTry(() -> {
+                        s1.send("cmd.chat", message); //给房间的每个人转发消息
+                    });
+                });
     }
 }

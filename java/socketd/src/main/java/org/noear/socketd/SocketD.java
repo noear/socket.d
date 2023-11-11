ebc8f596428d06c3b6763a50ac9efa3e009917ff
noear <noear@live.cn>
2023-11-11 10:32:54 +0800

    添加获取工厂的接口

--- java/socketd/src/main/java/org/noear/socketd/SocketD.java
+++ java/socketd/src/main/java/org/noear/socketd/SocketD.java
@@ -1,6 +1,7 @@
 package org.noear.socketd;
 
 import org.noear.socketd.transport.client.ClientFactory;
+import org.noear.socketd.transport.core.Asserts;
 import org.noear.socketd.transport.server.ServerFactory;
 import org.noear.socketd.transport.client.Client;
 import org.noear.socketd.transport.client.ClientConfig;
@@ -19,8 +20,8 @@ public class SocketD {
 
     /**
      * 版本版本号
-     * */
-    public static String version(){
+     */
+    public static String version() {
         return "2.0";
     }
 
@@ -44,10 +45,38 @@ public class SocketD {
         });
     }
 
+    public static String getConnSchema(String schema){
+        //支持 sd: 开头的架构
+        if(schema.startsWith("sd:")){
+            schema = schema.substring(3);
+        }
+        return schema;
+    }
+
+    /**
+     * 获取客户端工厂
+     */
+    public static ServerFactory getServerFactory(String schema) {
+        Asserts.assertNull(schema,"schema");
+
+        return serverFactoryMap.get(getConnSchema(schema));
+    }
+
+    /**
+     * 获取服务端工厂
+     */
+    public static ClientFactory getClientFactory(String schema) {
+        Asserts.assertNull(schema,"schema");
+
+        return clientFactoryMap.get(getConnSchema(schema));
+    }
+
     /**
      * 创建服务端
      */
     public static Server createServer(ServerConfig serverConfig) {
+        Asserts.assertNull(serverConfig,"serverConfig");
+
         ServerFactory factory = serverFactoryMap.get(serverConfig.getSchema());
         if (factory == null) {
             throw new IllegalStateException("No socketd server providers were found.");
@@ -62,6 +91,8 @@ public class SocketD {
      * @param serverUrl 服务器地址
      */
     public static Client createClient(String serverUrl) {
+        Asserts.assertNull(serverUrl,"serverUrl");
+
         ClientConfig clientConfig = new ClientConfig(serverUrl);
 
         ClientFactory factory = clientFactoryMap.get(clientConfig.getSchema());

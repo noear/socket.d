4f4aab0f98c118847c24bea25e9eb4c01f1626b4
noear <noear@live.cn>
2023-11-15 14:46:18 +0800

    添加 ssl 通讯测试代码（未通过）

---
+++ java/socketd-transport-test/src/test/java/features/cases/TestCase22_ssl.java
@@ -0,0 +1,125 @@
+package features.cases;
+
+import features.utils.SslContextFactory;
+import org.junit.jupiter.api.Assertions;
+import org.noear.socketd.SocketD;
+import org.noear.socketd.transport.core.Entity;
+import org.noear.socketd.transport.core.Message;
+import org.noear.socketd.transport.core.Session;
+import org.noear.socketd.transport.core.entity.StringEntity;
+import org.noear.socketd.transport.core.listener.SimpleListener;
+import org.noear.socketd.transport.server.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 以客户端为主的基本消息测试
+ *
+ * @author noear
+ * @since 2.0
+ */
+public class TestCase22_ssl extends BaseTestCase {
+    private static Logger log = LoggerFactory.getLogger(TestCase22_ssl.class);
+
+    public TestCase22_ssl(String schema, int port) {
+        super(schema, port);
+    }
+
+    private Server server;
+    private Session clientSession;
+
+    private AtomicInteger serverOnMessageCounter = new AtomicInteger();
+    private AtomicInteger clientSubscribeReplyCounter = new AtomicInteger();
+
+    private SSLContext getSSLContext() throws Exception {
+        return SslContextFactory.create("/ssl/demo_store.pfx", "PKCS12", "1234");
+    }
+
+    @Override
+    public void start() throws Exception {
+        log.trace("...");
+
+        SSLContext sslContext = getSSLContext();
+
+        super.start();
+        //server
+        server = SocketD.createServer(getSchema())
+                .config(c -> c.port(getPort()).sslContext(sslContext))
+                .listen(new SimpleListener() {
+                    @Override
+                    public void onMessage(Session session, Message message) throws IOException {
+                        System.out.println("::" + message);
+                        serverOnMessageCounter.incrementAndGet();
+
+                        if (message.isRequest()) {
+                            session.reply(message, new StringEntity("hi reply")); //这之后，无效了
+                            session.reply(message, new StringEntity("hi reply**"));
+                        }
+
+                        if (message.isSubscribe()) {
+                            session.reply(message, new StringEntity("hi reply"));
+                            session.reply(message, new StringEntity("hi reply**"));
+                            session.replyEnd(message, new StringEntity("hi reply****")); //这之后，无效了
+                            session.reply(message, new StringEntity("hi reply******"));
+                            session.reply(message, new StringEntity("hi reply********"));
+                        }
+
+                        session.send("demo", new StringEntity("test"));
+                    }
+                })
+                .start();
+
+        //休息下，启动可能要等会儿
+        Thread.sleep(1000);
+
+
+        //client
+        String serverUrl = getSchema() + "://127.0.0.1:" + getPort() + "/path?u=a&p=2";
+        clientSession = SocketD.createClient(serverUrl).config(c->c.sslContext(sslContext)).open();
+        clientSession.send("/user/created", new StringEntity("hi"));
+
+        Entity response = clientSession.sendAndRequest("/user/get", new StringEntity("hi"));
+        System.out.println("sendAndRequest====" + response);
+
+        clientSession.sendAndSubscribe("/user/sub", new StringEntity("hi"), message -> {
+            clientSubscribeReplyCounter.incrementAndGet();
+            System.out.println("sendAndSubscribe====" + message);
+        });
+
+        for (int i = 0; i < 3; i++) {
+            try {
+                Thread.sleep(100);
+                clientSession.send("/user/updated", new StringEntity("hi"));
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+
+        //休息下（发完，那边还得收）
+        Thread.sleep(1000);
+
+        System.out.println("counter: " + serverOnMessageCounter.get() + ", " + clientSubscribeReplyCounter.get());
+
+        Assertions.assertNotNull(response, getSchema() + ":sendAndRequest 返回不对");
+        Assertions.assertEquals("hi reply", response.dataAsString(), getSchema() + ":sendAndRequest 返回不对");
+        Assertions.assertEquals(serverOnMessageCounter.get(), 6, getSchema() + ":server 收的消息数量对不上");
+        Assertions.assertEquals(clientSubscribeReplyCounter.get(), 3, getSchema() + ":client 订阅回收数量对不上");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        if (clientSession != null) {
+            clientSession.close();
+        }
+
+        if (server != null) {
+            server.stop();
+        }
+
+        super.stop();
+    }
+}
---
+++ java/socketd-transport-test/src/test/java/features/utils/SslContextFactory.java
@@ -0,0 +1,72 @@
+package features.utils;
+
+import org.noear.socketd.utils.Utils;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+public class SslContextFactory {
+
+    public static SSLContext create(String keyStoreName, String keyStorePassword) throws IOException {
+        return create(keyStoreName, null, keyStorePassword);
+    }
+
+    public static SSLContext create(String keyStoreName, String keyStoreType, String keyStorePassword) throws IOException {
+        if (Utils.isEmpty(keyStoreType)) {
+            keyStoreType = "jks";
+        }
+
+        KeyStore keyStore = loadKeyStore(keyStoreName, keyStoreType, keyStorePassword);
+
+        KeyManager[] keyManagers = buildKeyManagers(keyStore, keyStorePassword.toCharArray());
+
+        SSLContext sslContext;
+
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(keyManagers, null, null);
+        } catch (NoSuchAlgorithmException | KeyManagementException exc) {
+            throw new IOException("Unable to create and initialise the SSLContext", exc);
+        }
+
+        return sslContext;
+    }
+
+    private static KeyStore loadKeyStore(final String location, String type, String storePassword)
+            throws IOException {
+
+        URL KeyStoreUrl = Utils.class.getResource(location);
+
+        if (KeyStoreUrl == null) {
+            throw new IllegalStateException("The keyStore file does not exist: " + location);
+        }
+
+        try (InputStream stream = KeyStoreUrl.openStream()) {
+            KeyStore loadedKeystore = KeyStore.getInstance(type);
+            loadedKeystore.load(stream, storePassword.toCharArray());
+            return loadedKeystore;
+        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException exc) {
+            throw new IOException(String.format("Unable to load KeyStore %s", location), exc);
+        }
+    }
+
+    private static KeyManager[] buildKeyManagers(final KeyStore keyStore, char[] storePassword)
+            throws IOException {
+        KeyManager[] keyManagers;
+        try {
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory
+                    .getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore, storePassword);
+            keyManagers = keyManagerFactory.getKeyManagers();
+        } catch (NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException exc) {
+            throw new IOException("Unable to initialise KeyManager[]", exc);
+        }
+        return keyManagers;
+    }
+}

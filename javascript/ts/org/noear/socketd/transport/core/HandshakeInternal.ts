f992f8e1e7a63586f55dee74449725c2a0487d41
noear <noear@live.cn>
2023-11-21 22:57:28 +0800

    删除自动生成的 ts 代码

--- javascript/ts/org/noear/socketd/transport/core/HandshakeInternal.ts
+++
@@ -1,9 +0,0 @@
-import {Handshake} from "./Handshake";
-import {MessageInternal} from "./MessageInternal";
-
-export interface HandshakeInternal extends Handshake {
-    /**
-     * 获取消息源
-     */
-    getSource(): MessageInternal;
-}
--- javascript/ts/org/noear/socketd/transport/core/MessageInternal.ts
+++
@@ -1,8 +0,0 @@
-import {Message} from "./Message";
-
-export interface MessageInternal extends Message {
-    /**
-     * 获取标记
-     */
-    flag(): number;
-}

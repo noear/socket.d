29723cfa6d685f6e048763a19c33857708aa1018
noear <noear@live.cn>
2023-12-04 11:18:38 +0800

    添加 sendAndRequest 异步单测

---
+++ java/socketd-transport-test/src/test/java/features/cases/TestCase26_sendAndRequest_async.java
@@ -0,0 +1,89 @@
+package features.cases;
+
+import org.junit.jupiter.api.Assertions;
+import org.noear.socketd.SocketD;
+import org.noear.socketd.transport.core.Message;
+import org.noear.socketd.transport.core.Session;
+import org.noear.socketd.transport.core.entity.StringEntity;
+import org.noear.socketd.transport.core.listener.SimpleListener;
+import org.noear.socketd.transport.server.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * sendAndRequest() 异步（一般在 Broker 里用）
+ *
+ * @author noear
+ * @since 2.0
+ */
+public class TestCase26_sendAndRequest_async extends BaseTestCase {
+    private static Logger log = LoggerFactory.getLogger(TestCase26_sendAndRequest_async.class);
+
+    public TestCase26_sendAndRequest_async(String schema, int port) {
+        super(schema, port);
+    }
+
+    private Server server;
+    private Session clientSession;
+
+    private AtomicInteger messageCounter = new AtomicInteger();
+
+    @Override
+    public void start() throws Exception {
+        log.trace("...");
+
+        super.start();
+        //server
+        server = SocketD.createServer(getSchema())
+                .config(c -> c.port(getPort()))
+                .listen(new SimpleListener() {
+                    @Override
+                    public void onMessage(Session session, Message message) throws IOException {
+                        System.out.println("::" + message);
+                        if (message.isRequest()) {
+                            messageCounter.incrementAndGet();
+                            session.reply(message, new StringEntity("Me too!"));
+                        }
+                    }
+                })
+                .start();
+
+        //休息下，启动可能要等会儿
+        Thread.sleep(1000);
+
+
+        //client
+        String serverUrl = getSchema() + "://127.0.0.1:" + getPort() + "/path?u=a&p=2";
+        clientSession = SocketD.createClient(serverUrl).open();
+
+        clientSession.send("/user/created", new StringEntity("hi"));
+
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        clientSession.sendAndRequest("/user/get", new StringEntity("hi"), r -> {
+            countDownLatch.countDown();
+        });
+
+        countDownLatch.await();
+
+        System.out.println("counter: " + messageCounter.get());
+        Assertions.assertEquals(messageCounter.get(), 1, getSchema() + ":server 收的消息数量对不上");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        if (clientSession != null) {
+            clientSession.close();
+        }
+
+        if (server != null) {
+            server.stop();
+        }
+
+        super.stop();
+    }
+}

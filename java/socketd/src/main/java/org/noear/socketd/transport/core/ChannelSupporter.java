f6d7625bd2e9bdea27ab3e27654a041a246eb123
noear <noear@live.cn>
2023-12-04 09:29:30 +0800

    优化注释

--- java/socketd/src/main/java/org/noear/socketd/transport/core/ChannelSupporter.java
+++ java/socketd/src/main/java/org/noear/socketd/transport/core/ChannelSupporter.java
@@ -1,12 +1,24 @@
 package org.noear.socketd.transport.core;
 
 /**
+ * 通道支持者（创建通道的参数）
+ *
  * @author noear
  * @since 2.1
  */
 public interface ChannelSupporter<S> {
+    /**
+     * 处理器
+     */
     Processor processor();
+
+    /**
+     * 配置
+     */
     Config config();
 
+    /**
+     * 通道助理
+     */
     ChannelAssistant<S> assistant();
 }

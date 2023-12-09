4986073885c1dff7e2360aa29fb3d51ec64c7aa8
noear <noear@live.cn>
2023-12-09 09:50:13 +0800

    添加大文件测试实验用例

--- java/socketd-transport-test/src/test/java/features/cases/TestCase24_bigFile_1g.java
+++ java/socketd-transport-test/src/test/java/features/cases/TestCase24_bigFile_1g.java
@@ -7,6 +7,7 @@ import org.noear.socketd.transport.core.EntityMetas;
 import org.noear.socketd.transport.core.Message;
 import org.noear.socketd.transport.core.Session;
 import org.noear.socketd.transport.core.entity.FileEntity;
+import org.noear.socketd.transport.core.fragment.FragmentHandlerTempfile;
 import org.noear.socketd.transport.core.listener.SimpleListener;
 import org.noear.socketd.transport.server.Server;
 import org.slf4j.Logger;
@@ -15,7 +16,7 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -46,7 +47,7 @@ public class TestCase24_bigFile_1g extends BaseTestCase {
         super.start();
         //server
         server = SocketD.createServer(getSchema())
-                .config(c -> c.port(getPort()))
+                .config(c -> c.port(getPort()).fragmentHandler(new FragmentHandlerTempfile()))
                 .listen(new SimpleListener() {
                     @Override
                     public void onMessage(Session session, Message message) throws IOException {
@@ -63,11 +64,13 @@ public class TestCase24_bigFile_1g extends BaseTestCase {
                             fileNew.createNewFile();
 
                             try {
-                                try (OutputStream outputStream = new FileOutputStream(fileNew)) {
-                                    outputStream.write(message.dataAsBytes());
+                                try (FileChannel fileChannel = new FileOutputStream(fileNew).getChannel()) {
+                                    fileChannel.write(message.data());
                                 }
                             } catch (Exception e) {
                                 e.printStackTrace();
+                            }finally {
+                                message.release();
                             }
                         }
 
---
+++ java/socketd-transport-test/src/test/java/labs/cae01_bigupload/BigUploadClient.java
@@ -0,0 +1,43 @@
+package labs.cae01_bigupload;
+
+import org.noear.socketd.SocketD;
+import org.noear.socketd.transport.client.ClientSession;
+import org.noear.socketd.transport.core.entity.FileEntity;
+
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
+public class BigUploadClient {
+
+    static final String[] schemas = new String[]{
+            "sd:tcp-java", "sd:tcp-netty", "sd:tcp-smartsocket",
+            "sd:ws-java",
+            "sd:udp-java"};
+
+    static String getSchema() {
+        return schemas[2];
+    }
+
+    static int getPort() {
+        return 2100;
+    }
+
+    public static void main(String[] args) throws Exception {
+        RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
+        String pid = rb.getName().split("@")[0];
+        System.out.println("pid=" + pid);
+
+
+        String serverUrl = getSchema() + "://127.0.0.1:" + getPort() + "/path?u=a&p=2";
+
+        ClientSession clientSession = SocketD.createClient(serverUrl)
+                .config(c -> c.fragmentSize(1024 * 1024)) //1m
+                .open();
+        int count = 0;
+        while (true) {
+            clientSession.sendAndRequest("/user/upload", new FileEntity(new File("/Users/noear/Movies/[Socket.D 实战] 直播手写 FolkMQ (4).mov")));
+            System.out.println("count=" + (count++));
+        }
+    }
+}

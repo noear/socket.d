115bcee76061870c5577c87cc7cc12d8992b83dc
noear <noear@live.cn>
2023-12-09 20:34:26 +0800

    添加配置 null 过滤

--- java/socketd/src/main/java/org/noear/socketd/transport/client/ClientBase.java
+++ java/socketd/src/main/java/org/noear/socketd/transport/client/ClientBase.java
@@ -84,8 +84,10 @@ public abstract class ClientBase<T extends ChannelAssistant> implements Client {
      * 配置
      */
     @Override
-    public Client config(ClientConfigHandler consumer) {
-        consumer.clientConfig(config);
+    public Client config(ClientConfigHandler configHandler) {
+        if (configHandler != null) {
+            configHandler.clientConfig(config);
+        }
         return this;
     }
 
@@ -94,7 +96,9 @@ public abstract class ClientBase<T extends ChannelAssistant> implements Client {
      */
     @Override
     public Client listen(Listener listener) {
-        processor.setListener(listener);
+        if (listener != null) {
+            processor.setListener(listener);
+        }
         return this;
     }
 

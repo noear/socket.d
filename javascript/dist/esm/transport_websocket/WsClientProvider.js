97f4cd632acaaca7ec00e86c9e71a17bc12cb7ab
noear <noear@live.cn>
2024-01-01 13:13:32 +0800

    javascript: 调整打包配置

--- javascript/dist/esm/transport_websocket/WsClientProvider.js
+++
@@ -1,9 +0,0 @@
-import { WsClient } from "./WsClient";
-export class WsClientProvider {
-    schemas() {
-        return ["ws", "wss", "sd:ws", "sd:wss"];
-    }
-    createClient(clientConfig) {
-        return new WsClient(clientConfig);
-    }
-}

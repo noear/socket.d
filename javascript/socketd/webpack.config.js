bf47fc6a33fbc97bd53732271506a84f6602e87b
aoshiguchen <1052045476@qq.com>
2024-01-04 10:42:30 +0800

    1、废弃在源码中将SocketD实例挂载出去的做法，改为由webpack配置实现挂载 2、修改webpack配置，支持通过环境变量指定挂载方式 3、修改package.json，增加针对node、浏览器的编译命令

--- javascript/socketd/webpack.config.js
+++ javascript/socketd/webpack.config.js
@@ -1,17 +1,39 @@
 const path = require('path');
 
-module.exports = {
-    // JavaScript 执行入口文件
-    entry: './dist/build/SocketD.js',
-    mode: 'production', //'none' | 'development' | 'production'
-    //target: 'node', //'node' | 'web'
-    experiments: {
-        outputModule: true,
-    },
-    output: {
-        // 把所有依赖的模块合并输出到一个 bundle.js 文件
-        filename: 'socket.d.js',
-        // 输出文件都放到 dist 目录下
-        path: path.resolve(__dirname, './dist/release')
-    }
-};
+module.exports = env => {
+    console.log('env11', env)
+
+    return {
+        // JavaScript 执行入口文件
+        entry: './dist/build/SocketD.js',
+        mode: 'production', //'none' | 'development' | 'production'
+        //target: 'node', //'node' | 'web'
+        experiments: {
+            outputModule: true,
+        },
+        output: {
+            // 把所有依赖的模块合并输出到一个 bundle.js 文件
+            filename: 'socket.d.js',
+            // 输出文件都放到 dist 目录下
+            path: path.resolve(__dirname, './dist/release'),
+            libraryTarget: env.LIB_TARGET || 'window'
+        }
+    };
+}
+
+// module.exports = {
+//     // JavaScript 执行入口文件
+//     entry: './dist/build/SocketD.js',
+//     mode: 'production', //'none' | 'development' | 'production'
+//     //target: 'node', //'node' | 'web'
+//     experiments: {
+//         outputModule: true,
+//     },
+//     output: {
+//         // 把所有依赖的模块合并输出到一个 bundle.js 文件
+//         filename: 'socket.d.js',
+//         // 输出文件都放到 dist 目录下
+//         path: path.resolve(__dirname, './dist/release'),
+//         libraryTarget: env.LIB_TARGET || 'window'
+//     }
+// };

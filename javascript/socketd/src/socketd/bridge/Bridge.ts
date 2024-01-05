3aefa26ed64712cbcdd1e92c98e0371235662eca
aoshiguchen <1052045476@qq.com>
2024-01-05 15:37:00 +0800

    js版本兼容node调整

---
+++ javascript/socketd/src/socketd/bridge/Bridge.ts
@@ -0,0 +1,13 @@
+import {EnvUtils} from '../utils/EnvUtils';
+import {BrowserWsClientImpl} from './impl/BridgeWsClientImpl.browser';
+import {NodeWsClientImpl} from './impl/BridgeWsClientImpl.node';
+import {BridgeWsClientConnector} from "./BridgeWsClient";
+
+export function createWsClient(url: string, connector: BridgeWsClientConnector) {
+    if (EnvUtils.isRunInBrowser()) {
+        console.log('浏览器端ws');
+        return new BrowserWsClientImpl(url, connector);
+    }
+    console.log('node端ws');
+    return new NodeWsClientImpl(url, connector);
+}

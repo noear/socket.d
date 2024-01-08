faaa19628f97e967ca3a965f78d196bb636c6dc0
zq <1091262307@qq.com>
2024-01-09 00:50:49 +0800

    uniapp中使用socket.d的demo

---
+++ javascript/socketd-examples/demo03-uniapp-ws/App.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+	onLaunch: function () {
+		console.log('App Launch')
+
+	},
+	onShow: function () {
+		console.log('App Show');
+
+
+	},
+	onHide: function () {
+		console.log('App Hide')
+
+	},
+	methods: {
+		
+	}
+}
+
+</script>
+
+<style>
+/*每个页面公共css */
+</style>

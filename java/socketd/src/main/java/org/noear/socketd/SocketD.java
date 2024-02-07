8468a37a832aa9d099f7d2325831e7c2b56a4888
noear <noear@live.cn>
2024-02-07 19:04:56 +0800

    2.4.0-SNAPSHOT

--- java/socketd/src/main/java/org/noear/socketd/SocketD.java
+++ java/socketd/src/main/java/org/noear/socketd/SocketD.java
@@ -24,7 +24,7 @@ public class SocketD {
      * 框架版本号
      */
     public static String version() {
-        return "2.3.12";
+        return "2.4.0-SNAPSHOT";
     }
 
     /**
--- javascript/socketd/webpack.md
+++ javascript/socketd/webpack.md
@@ -39,7 +39,7 @@ npm publish
 ```javascript
 
 /*!
- * Socket.D v2.3.12
+ * Socket.D v2.4.0-SNAPSHOT
  * (c) 2023-2024 noear.org
  * Released under the Apache-2.0 License.
  */

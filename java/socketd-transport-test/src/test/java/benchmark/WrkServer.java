83c10cf657a9fb7f8e0631ce054fafbe31babe0d
noear <noear@live.cn>
2024-03-17 12:28:43 +0800

    java: 添加 wrk 测试场景

---
+++ java/socketd-transport-test/src/test/java/benchmark/WrkServer.java
@@ -0,0 +1,61 @@
+package benchmark;
+
+import org.noear.socketd.SocketD;
+import org.noear.socketd.transport.core.Message;
+import org.noear.socketd.transport.core.Session;
+import org.noear.socketd.transport.core.entity.StringEntity;
+import org.noear.socketd.transport.core.listener.SimpleListener;
+import org.noear.socketd.transport.server.Server;
+import org.noear.solon.Solon;
+import org.noear.solon.core.bean.LifecycleBean;
+
+import java.io.IOException;
+
+/**
+ * @author noear
+ * @since 2.4
+ */
+public class WrkServer implements LifecycleBean {
+    public static final int schemasIdx = 3;
+    public static final String[] schemas = new String[]{
+            "sd:tcp-java",
+            "sd:tcp-netty",
+            "sd:tcp-smartsocket",
+            "sd:ws-java",
+            "sd:udp-java",
+            "sd:udp-netty",
+            "sd:kcp-java",
+    };
+
+    public static void main(String[] args) {
+        Solon.start(WrkServer.class, args, app -> {
+            app.enableHttp(false);
+            app.context().lifecycle(new WrkServer());
+        });
+    }
+
+    Server server;
+
+    @Override
+    public void start() throws Throwable {
+        String s = schemas[schemasIdx];
+
+        server = SocketD.createServer(s)
+                .config(c -> c.port(8602))
+                .listen(new SimpleListener() {
+                    @Override
+                    public void onMessage(Session session, Message message) throws IOException {
+                        if (message.isRequest() || message.isSubscribe()) {
+                            String name = message.meta("name");
+                            session.replyEnd(message, new StringEntity(name + ": me to!"));
+                        }
+                    }
+                })
+                .start();
+    }
+
+    @Override
+    public void stop() throws Throwable {
+        server.stop();
+    }
+}
---
+++ java/socketd-transport-test/src/test/java/benchmark/WrkTest.java
@@ -0,0 +1,52 @@
+package benchmark;
+
+import org.noear.socketd.SocketD;
+import org.noear.socketd.transport.client.ClientSession;
+import org.noear.socketd.transport.core.Entity;
+import org.noear.socketd.transport.core.entity.StringEntity;
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.core.event.AppBeanLoadEndEvent;
+import reactor.core.publisher.Mono;
+
+/**
+ * 这个测试主要测会不会被压死！？
+ *
+ * @author noear
+ * @since 2.4
+ */
+public class WrkTest {
+    public static void main(String[] args) {
+        Solon.start(WrkTest.class, args, app -> {
+            app.onEvent(AppBeanLoadEndEvent.class, e -> {
+                app.add("/", WrkTest.class);
+            });
+        });
+    }
+
+    private final ClientSession clientSession;
+
+    public WrkTest() {
+        String s = WrkServer.schemas[WrkServer.schemasIdx];
+        clientSession = SocketD.createClient(s + "://127.0.0.1:8602").open();
+    }
+
+    @Mapping("/rx")
+    public Mono<String> rx(String name) throws Exception {
+        return Mono.create(sink -> {
+            try {
+                Entity entity = new StringEntity("hello")
+                        .metaPut("name", name == null ? "noear" : name);
+
+                clientSession.sendAndRequest("hello", entity).thenReply(reply -> {
+                    sink.success(reply.dataAsString());
+                }).thenError(e -> {
+                    sink.error(e);
+                });
+            } catch (Throwable e) {
+                sink.error(e);
+            }
+        });
+    }
+
+}

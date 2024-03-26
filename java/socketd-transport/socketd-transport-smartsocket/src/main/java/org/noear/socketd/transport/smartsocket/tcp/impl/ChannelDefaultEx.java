f9319a56721dea9fb363b89dc698803f17c56177
noear <noear@live.cn>
2024-03-26 20:54:55 +0800

    java: smartsocket 升为 1.5.43

--- java/socketd-transport/socketd-transport-smartsocket/src/main/java/org/noear/socketd/transport/smartsocket/tcp/impl/ChannelDefaultEx.java
+++ java/socketd-transport/socketd-transport-smartsocket/src/main/java/org/noear/socketd/transport/smartsocket/tcp/impl/ChannelDefaultEx.java
@@ -3,7 +3,6 @@ package org.noear.socketd.transport.smartsocket.tcp.impl;
 import org.noear.socketd.transport.core.ChannelSupporter;
 import org.noear.socketd.transport.core.impl.ChannelDefault;
 import org.smartboot.socket.extension.decoder.FixedLengthFrameDecoder;
-import org.smartboot.socket.transport.AioSession;
 
 /**
  * 通道扩展（增加固定附件的能力）

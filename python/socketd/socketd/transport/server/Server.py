50f40f2248778213baa3f3fc9dcb50f7be28d46d
noear <noear@live.cn>
2024-04-18 13:47:32 +0800

    py: Server 接口添加注释

--- python/socketd/socketd/transport/server/Server.py
+++ python/socketd/socketd/transport/server/Server.py
@@ -7,18 +7,25 @@ from websockets.sync.server import WebSocketServer
 from socketd.transport.server.ServerConfig import ServerConfig
 from socketd.transport.core.Listener import Listener
 
-
+#服务端接口
 class Server:
+    #获取台头
     def get_title(self): ...
 
+    #获取配置
     def get_config(self) -> ServerConfig: ...
 
+    #配置
     def config(self, consumer: Callable[[ServerConfig], None]) -> Server: ...
 
+    #监听
     def listen(self, listener: Listener) -> Server: ...
 
+    #启动
     def start(self) -> WebSocketServer | Coroutine: ...
 
+    #预停止
     def prestop(self) -> Coroutine: ...
 
+    #停止
     def stop(self) -> Coroutine: ...

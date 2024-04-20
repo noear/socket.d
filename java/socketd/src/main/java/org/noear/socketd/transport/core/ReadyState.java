ca37c4f463b167536aca24657d121ac3531b096d
noear <noear@live.cn>
2024-04-20 11:30:48 +0800

    java: 添加 ReadyState

--- java/socketd/src/main/java/org/noear/socketd/transport/core/ReadyState.java
+++ java/socketd/src/main/java/org/noear/socketd/transport/core/ReadyState.java
@@ -1,12 +1,12 @@
 package org.noear.socketd.transport.core;
 
 /**
- * 状态
+ * 状态状态
  *
  * @author noear
  * @since 2.4
  */
-public enum ReadyStatus {
+public enum ReadyState {
     CONNCETION,
     OPEN,
     CLOSING,

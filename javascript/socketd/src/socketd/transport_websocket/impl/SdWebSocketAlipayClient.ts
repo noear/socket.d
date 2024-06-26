f98bcaefb9398ee5136adeaa9983d2733143f01d
noear <noear@live.cn>
2024-06-26 08:43:33 +0800

    js: 添加 SdWebSocketAlipayClient

---
+++ javascript/socketd/src/socketd/transport_websocket/impl/SdWebSocketAlipayClient.ts
@@ -0,0 +1,96 @@
+import {
+    SdWebSocket,
+    SdWebSocketListener,
+    SdWebSocketEventImpl,
+    SdWebSocketMessageEventImpl,
+    SdWebSocketCloseEventImpl,
+    SdWebSocketState,
+    SdWebSocketErrorEventImpl
+} from "./SdWebSocket";
+import {SocketAddress} from "../../transport/core/SocketAddress";
+import {SocketD} from "../../SocketD";
+
+export class SdWebSocketAlipayClient implements SdWebSocket {
+    private _real: any;
+    private _state: SdWebSocketState;
+    private _listener: SdWebSocketListener;
+
+    constructor(url: string, listener: SdWebSocketListener) {
+        this._state = SdWebSocketState.CONNECTING;
+        // @ts-ignore
+        this._real = my.connectSocket({url: url, protocols:[SocketD.protocolName()], multiple:true});//SocketTask
+        this._listener = listener;
+        this._real.binaryType = "arraybuffer";
+
+        this._real.onOpen(this.onOpen.bind(this));
+        this._real.onMessage(this.onMessage.bind(this));
+        this._real.onClose(this.onClose.bind(this));
+        this._real.onError(this.onError.bind(this));
+    }
+
+    remoteAddress(): SocketAddress|null {
+        return null;
+    }
+    localAddress(): SocketAddress | null {
+        return null;
+    }
+
+    private _attachment:any;
+    attachment() {
+        return this._attachment;
+    }
+    attachmentPut(data: any) {
+        this._attachment = data;
+    }
+
+    isConnecting(): boolean {
+        return this._state == SdWebSocketState.CONNECTING;
+    }
+
+    isClosed(): boolean {
+        return this._state == SdWebSocketState.CLOSED;
+    }
+
+    isClosing(): boolean {
+        return this._state == SdWebSocketState.CLOSING;
+    }
+
+    isOpen(): boolean {
+        return this._state == SdWebSocketState.OPEN;
+    }
+
+    onOpen(e: Event) {
+        let evt = new SdWebSocketEventImpl(this);
+        this._state = SdWebSocketState.OPEN;
+        this._listener.onOpen(evt);
+    }
+
+    onMessage(e: MessageEvent) {
+        let evt = new SdWebSocketMessageEventImpl(this, e.data);
+        this._listener.onMessage(evt);
+    }
+
+    onClose(e: CloseEvent) {
+        let evt = new SdWebSocketCloseEventImpl(this);
+        this._state = SdWebSocketState.CLOSED;
+        this._listener.onClose(evt);
+    }
+
+    onError(e) {
+        let evt = new SdWebSocketErrorEventImpl(this, e);
+        this._listener.onError(evt);
+    }
+
+    close(): void {
+        this._state = SdWebSocketState.CLOSING;
+        this._real.close({
+            complete: () => {
+                this._state = SdWebSocketState.CLOSED;
+            }
+        });
+    }
+
+    send(data: string | ArrayBuffer): void {
+        this._real.send({data: data});
+    }
+}

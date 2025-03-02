1e65784128ba89203098196092d719333ac168e9
noear <noear@live.cn>
2025-03-02 20:05:28 +0800

    2.5.16

--- java/socketd/src/main/java/org/noear/socketd/SocketD.java
+++ java/socketd/src/main/java/org/noear/socketd/SocketD.java
@@ -25,7 +25,7 @@ public class SocketD {
      * 框架版本号
      */
     public static String version() {
-        return "2.5.15";
+        return "2.5.16";
     }
 
     /**
--- javascript/socketd/webpack.config.js
+++ javascript/socketd/webpack.config.js
@@ -8,9 +8,11 @@ module.exports = {
     },
     output: {
         // 把所有依赖的模块合并输出到一个 bundle.js 文件
-        filename: 'socket.d.js', //window -> socket.d.js // module->socket.d.esm.js (要去掉最后的 r as __esModule)
+        // filename: 'socket.d.esm.js', //window -> socket.d.js // module->socket.d.esm.js (要去掉最后的 r as __esModule)
+        filename: 'socket.d.js',
         // 输出文件都放到 dist 目录下
         path: path.resolve(__dirname, './dist/release'),
-        libraryTarget: 'window'//'commonjs2' //module //window //global//this(更通用)
+        // libraryTarget: 'module'//'commonjs2' //module //window //global//this(更通用)
+        libraryTarget: 'window'
     }
 }

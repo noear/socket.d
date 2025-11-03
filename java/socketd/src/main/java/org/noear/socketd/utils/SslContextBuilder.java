c78e0ad09d763a68124dd698cfb8f3af0f1ccbaa
noear <noear@live.cn>
2025-11-03 08:17:52 +0800

    添加 SslContextBuilder:trustManagerAsEmpty

--- java/socketd/src/main/java/org/noear/socketd/utils/SslContextBuilder.java
+++ java/socketd/src/main/java/org/noear/socketd/utils/SslContextBuilder.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * Ssl 上下文构建器
@@ -70,10 +71,25 @@ public class SslContextBuilder {
         return this;
     }
 
+    public SslContextBuilder trustManagerAsEmpty() {
+        trustManagers = new TrustManager[]{new EmptyX509TrustManager()};
+        return this;
+    }
+
     /**
      * 构建
      */
     public SSLContext build() throws NoSuchAlgorithmException, KeyManagementException {
+        // if client:
+        //  keyManagers == null, 表示客户端不提供客户端证书,用于单向认证场景（只需要服务器证书）,系统会使用默认的 KeyManager
+        //  trustManagers == null, 表示使用默认的信任库（通常是 JRE 的 cacerts）,信任所有标准的 CA 机构颁发的证书
+        //  secureRandom == null, 表示使用系统默认的随机数生成器,在大多数情况下，使用默认的就已经足够安全
+        //
+        // if server:
+        //  keyManagers != null, 服务端必须提供证书来向客户端证明身份
+        //  trustManagers == null, 表示不需要验证客户端证书
+        //  secureRandom == null, 与客户端意思相同
+
         SSLContext sslContext = SSLContext.getInstance(protocol);
         sslContext.init(keyManagers, trustManagers, secureRandom);
 
@@ -84,5 +100,24 @@ public class SslContextBuilder {
         KeyStore ks = KeyStore.getInstance(keyStoreType);
         ks.load(keyStoreInputStream, password);
         return ks;
+    }
+
+    private static final class EmptyX509TrustManager implements X509TrustManager {
+        private final X509Certificate[] acceptedIssuers = new X509Certificate[0];
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return acceptedIssuers;
+        }
     }
 }
